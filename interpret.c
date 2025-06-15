25c9db7197fc194b7f9577c37795e668ad324241
Martins98725 <icaro25martins@gmail.com>
2025-06-15 16:44:50 -0300

    fix: testando todas as funcionalidades do código

--- interpret.c
+++ interpret.c
@@ -18,9 +18,8 @@ void interpret(const char *source)
     char op[10];
     char arg[10];
 
-     sscanf(source, "%s%s", op, arg);
+    sscanf(source, "%s%s", op, arg);
 
-   
     if (strcmp(op, "push") == 0)
     {
         int value = atoi(arg);
@@ -71,41 +70,38 @@ void interpret(const char *source)
 
         stack_push(stack, resultado);
     }
-        
+
     else if (strcmp(op, "pop") == 0)
     {
         int value = stack_pop(stack);
         if (value != -1)
             printf("Valor desempilhado: %d\n", value);
-    } 
-        
-    else if (strcmp(op,"div") == 0){
-      int numero1 = stack_pop(stack);
-      int numero2 = stack_pop(stack);
+    }
 
-        if (numero1 == -1 || numero2 == -1) {
-        printf("Erro: pilha com menos de 2 elementos.\n");
-        return;
-        }
-    
-        if (numero1 == 0){
-        printf("Erro: divisão por zero.\n");
-        return;
-        }
-        
-      int resultado = numero1/numero2;
-      stack_push(stack, resultado);
-     } 
+    else if (strcmp(op, "div") == 0)
+    {
+        int numero1 = stack_pop(stack);
+        int numero2 = stack_pop(stack);
 
-    else if (strcmp(op,"print") == 0){
-      printf("%d\n", stack_pop(stack));
-     } 
-    
+        if (numero1 == -1 || numero2 == -1)
+        {
+            printf("Erro: pilha com menos de 2 elementos.\n");
+
+            return;
+        }
+        int resultado = (numero2 / numero1);
+        stack_push(stack, resultado);
+    }
+
+    else if (strcmp(op, "print") == 0)
+    {
+
+        stack_print(stack);
+    }
+
     else
     {
         printf("Operação desconhecida: %s\n", op);
         return;
     }
-    
-      stack_print(stack);
 }
